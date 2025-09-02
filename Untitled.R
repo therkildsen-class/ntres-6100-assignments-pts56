52afd9517921a8c4ef7d61de5cb6502260b39676
pts56 <pts56@cornell.edu>
2025-09-02 19:42:08 -0400

    This is from earler. not improtasnt

---
+++ Untitled.R
@@ -0,0 +1,8 @@
+library(usethis) #install.packages("usethis")
+library(gitcreds) #install.packages("gitcreds")
+
+
+use_git_config(user.name = "pts56", user.email = "pts56@cornell.edu")
+
+
+create_github_token()
