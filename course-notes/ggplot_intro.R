6fbc0b5d7eccbca401b92a41d7d74cb3f536407d
pts56 <pts56@cornell.edu>
2025-09-11 11:23:58 -0400

    again

--- course-notes/ggplot_intro.R
+++ course-notes/ggplot_intro.R
@@ -8,7 +8,21 @@ plot(mpg$hwy,mpg$displ)#class "r" scatter plot i just learned in BTRY
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy, # x and y axis 
                            color = class, size = cyl), # aesthetics 
-             shape = 1). #makes the circles empty 
+             shape = 1) + #makes the circles empty 
+  geom_smooth(mapping = aes(x = displ, y= hwy)) + #be careful to always include + 
+  facet_wrap(~ year, nrow = 2) 
+
+
+ggplot(data=mpg,(mapping = aes(x = displ, y= hwy)) + 
+         geom_point(mapping = aes(x = displ, y = hwy, # x and y axis 
+                    color = class, size = cyl), # aesthetics 
+                    shape = 1) + #makes the circles empty 
+         geom_smooth() + 
+         facet_wrap(~ year, nrow = 2)
+
+
+
+
 
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = hwy, y = cyl))
@@ -16,5 +30,16 @@ ggplot(data = mpg) +
 ggplot(data = mpg) + 
     geom_point(mapping = aes(x = cyl, y = hwy))##changing x and y axis 
 
+
+## Exercise 3 
 ggplot(data = mpg) + 
-  geom_point(mapping = aes(x = class, y = drv, color = class, size = drv))
+  geom_point(mapping = aes(x = displ, y = hwy),
+             color = "blue") # make sure to put color outside of the inital () 
+
+
+
+
+
+
+
+
