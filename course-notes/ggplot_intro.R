cc11cebf737df80cc4139a57ac609efe426bc125
pts56 <pts56@cornell.edu>
2025-09-11 11:05:21 -0400

    mid class push

--- course-notes/ggplot_intro.R
+++ course-notes/ggplot_intro.R
@@ -2,6 +2,19 @@ library(tidyverse)
 view(mpg)
 #gg is for the grammar of graphics 
 #in ggg plot is built from a series of data (Data-Aesthetics-Geometries_Facets_Stats-Coordinates-Theme)
-plot(mpg$hwy,mpg$displ)#class "r" scatterplot i just learned in BTRY 
+plot(mpg$hwy,mpg$displ)#class "r" scatter plot i just learned in BTRY 
 
 
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, # x and y axis 
+                           color = class, size = cyl), # aesthetics work 
+             shape = 1). #makes the circles empty 
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = hwy, y = cyl))
+
+ggplot(data = mpg) + 
+    geom_point(mapping = aes(x = cyl, y = hwy))##changing x and y axis 
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = class, y = drv, color = class, size = drv))
