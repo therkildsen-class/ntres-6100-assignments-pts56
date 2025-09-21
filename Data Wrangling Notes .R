8ddbf18da986a0524511ddb4c206658bb93d8135
pts56 <pts56@cornell.edu>
2025-09-21 19:30:43 -0400

    bb

--- Data Wrangling Notes .R
+++ Data Wrangling Notes .R
@@ -6,6 +6,7 @@
 library(tidyverse) #building good habits by loading package at beginning of script 
 library(skimr) #install.packages("skimr")
 coronavirus <- read_csv('https://raw.githubusercontent.com/RamiKrispin/coronavirus/master/csv/coronavirus.csv')
+vacc <- read_csv("https://raw.githubusercontent.com/RamiKrispin/coronavirus/main/csv/covid19_vaccine.csv")
 
 ## list of handy functions to view data 
 summary(coronavirus) #helpful way to initially view data 
@@ -48,7 +49,7 @@ filter(coronavirus, country=='US' & type=="death") ## record of Death cases from
 ##Exercise 1: What is the total number of deaths in the US included in this dataset 
 #1b: Subset the dta to nly show the death counts in European countries on today's date io 2021 
 
-filter(coronavirus, country%in%c("Germany","Austria","Poland"), type=="death", date=="2021-09-16")#filder does rows
+filter(coronavirus, country%in%c("Germany","Austria","Poland"), type=="death", date=="2021-09-16")#filter does rows
 
 #select does columns 
 select(coronavirus, date, country, type, cases) ## how to select specific columns THE ORDER HERE MATTERS
@@ -58,8 +59,76 @@ select(coronavirus, -province)
 
 select(coronavirus, country, lat, long)
 select(coronavirus, lat, long, country)
-
-
+select(coronavirus, date:cases)
+select(coronavirus, 1:3)
 View(count(coronavirus, country))## see how it spells different countries 
+select(coronavirus, contains('y'), everything()) ## "Reorder" and "Rename" are also quite helpful 
+
+filter(coronavirus, country%in%c("US"))
+coronavirus_US <- filter(coronavirus, country%in%c("US")) 
+
+#select(coronavirus_US, -lat, -long, -province) ### - means drop  
+#coronavirus_US <- select(coronavirus_US, -lat, -long, -province) ### - means drop  
+
+## really important idea of tidyverse coding is the idea of using a pipe operator 
+
+|> ### keyboard shotcut is shift+command+m "and then" 
+  
+coronavirus |> #"and then" 
+  filter (country == "US") |> 
+  select(-lat, -long, -province) #line breaks don't matter but makes code a lot more readable. new operaroin per line
+
+#without the pipe we have to start "nesting" functions within themselves 
+
+## use pipe function to only inlcude death counts in Us, Canada, Mexico
+
+coronavirus |> #"and then"
+  filter(type=="death",  ## remember to use == when filtering by type
+         country %in%c("US", "Mexico", "Canada")) |>  ## by death
+  select(country, date, cases) |> # here we do not use quotation marks 
+  ggplot() + 
+  geom_line(mapping = aes(x = date, y = cases, color = country)) 
+
+
+# mutate() function ... adds new variables 
+
+vacc |> 
+  filter(date == max(date)) |> # "hard code" means typing a specific text string 
+  select(country_region, continent_name, people_at_least_one_dose, population) |> 
+  mutate(vaxxrate = round(people_at_least_one_dose / population, 2)) #the fist argument it needs is "what it is operating on"
+## mutate function creates a new variable 
+
+
+Exercise 2 
+#add a new varibles that shows how many doses of vaccine on agerage have been distributed per country
+#make a new variable, # of doses adinstrated/poeple with at least one dose()
+vacc |> 
+  filter(date==max(date)) |> 
+  select(country_region, continent_name, people_at_least_one_dose, doses_admin, population) |> 
+  mutate(doses_per_vaxxed = round(doses_admin/people_at_least_one_dose, 2))|> 
+  filter(doses_per_vaxxed > 3) |> 
+  arrange(-doses_per_vaxxed) #"r" version of excel sort 
+
+
+  filter(doses_admin > 200 * 10^6) |> 
+  ggplot() + 
+  geom_histogram(mapping = aes( x=doses_per_vaxxed)
+  
+
+ #Exercise 3> which countries have the higest vaccination rates 
+#what is the variable we need to create in order to answer this question? 
+vacc |> 
+  filter(date==max(date)) |> 
+  select(country_region, continent_name, people_at_least_one_dose, doses_admin, population) |> 
+  mutate(vaxxrate = people_at_least_one_dose/population) |> 
+  filter(vaxxrate > 0.9) |> 
+  arrange(-vaxxrate) |> 
+  ## vaeriables are column headers and they're not going to show up in our enviorment 
+
+
+
+
+
+
 
 
