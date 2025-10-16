9b45ed67de382ed076be5174e107ccc7dfbf0659
pts56 <pts56@cornell.edu>
2025-10-16 11:08:20 -0400

    .

--- Notes 10.16.25.R
+++ Notes 10.16.25.R
@@ -9,30 +9,33 @@ library(gapminder)
 
 ##four loop (If and ifelse)
 
+view(gapminder)
 
 
 
-cntry <- "Belgium"
 
+country_list <- c("Chile", "Argentina", "Canada" )
+country_list <- unique(gapminder$country)
 
-for (each_country in list_of_countries) { ## our for loop set up 
+dir.create("figures")
 
-gap_to_plot<-  gapminder |> 
-  filter(country == cntry)
-gap_to_plot_afg
-
-my_plot <- ggplot(data = gap_to_plot_afg,
-                  mapping = aes(x = year,
-                                y = gdp_per_cap)) +
-  geom_point() + 
-  labs(title = str_c(cntry, "GDP per cap", sep = " ")) 
-
-my_plot
-
-ggsave(filename = str_c(cntry, "_gdp_per_gap.png", sep = "", plot = my_plot)
-
-
+for (cntry in country_list) {
+  
+  gap_to_plot<-  gapminder |> 
+    filter(country == cntry)
+  gap_to_plot
+  
+  my_plot <- ggplot(data = gap_to_plot,
+                    mapping = aes(x = year,
+                                  y = gdp_per_cap)) +
+    geom_point() + 
+    labs(title = str_c(cntry, "GDP per cap", sep = " ")) +
+    ggsave(filename = str_c("figures/", cntry, "_gdp_per_gap.png", sep = "", plot = my_plot)
 }
+
+
+
+
 
 
 
