14649342c64033b298096ca2cb720f6beb739198
pts56 <pts56@cornell.edu>
2025-10-16 11:07:35 -0400

    ,

---
+++ 10.16 Excersize 2 .R
@@ -0,0 +1,21 @@
+for (cntry in country_list) {
+  
+  ## filter the country to plot
+  gap_to_plot <- gapminder |>
+    filter(country == cntry)
+  
+  ## plot
+  my_plot <- ggplot(data = gap_to_plot, aes(x = year, y = gdp_per_cap)) + 
+    geom_point() +
+    ## add title and save
+    labs(title = str_c(cntry, 
+                       "GDP per capita", 
+                       sep = " "))
+  
+  ## add the gap_figs/ folder
+  ggsave(filename = str_c("gap_figs/", cntry, 
+                          "_gdp_per_cap.png", 
+                          sep = ""), 
+         plot = my_plot)
+} 
+
