304110d2cdd389b645196ffc852f1012dbd126f7
pts56 <pts56@cornell.edu>
2025-10-16 10:24:08 -0400

    ..

---
+++ Notes 10.16.25.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+# Lesson 15: iteration Part 1: 
+#announcements 
+#on october 30th we'll have end of course presentations 
+# just posted updated versions of assignments 8 and 9 
+#key programming concepts: iteratoin and conditional excution 
+#what do do if we have to do the same thing over and over again
+
+##four loop (If and ifelse)
+
